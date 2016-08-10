b6a070e475609db23925f7935d4d539a9885bb2f
Megan <meganemilyduffy@gmail.com>
2016-08-10 14:21:28 +0100

    Added iTunes API

--- js/controller.js
+++ js/controller.js
@@ -4,6 +4,7 @@ bandApp.controller("HomeController", function($scope) {
 
 bandApp.controller("MusicController", function($scope) {
 
+
 });
 
 bandApp.controller("GigsController", function($scope) {
@@ -32,4 +33,34 @@ bandApp.controller("BookController", function($scope, $location) {
 
 bandApp.controller("BookConfirmController", function($scope) {
 	$scope.title= "Booking Confirmation";
+});
+
+bandApp.controller("iTunesController", function($scope, $http) {
+	//define search function called by form
+	$scope.searchiTunes = function(artist) {
+		//use the jsonp callback function from the $http service this
+		$http.jsonp("http://itunes.apple.com/search?limit=15", {
+			params: {
+				"callback" : "JSON_CALLBACK",
+				"term" : artist
+			}
+			//returns a promise. when resturned, .then preform action..
+		}).then(onSearchComplete, onError)
+	}
+	//get the data out of the response when search succeeds.
+
+	var onSearchComplete = function(response) {
+		//the response has a few data elements
+		//so we will grab all of that
+		$scope.data = response.data
+
+		//we will also store just the songs into 
+		//a seperate variable for the view to iterate
+		$scope.songs = response.data.results
+	}
+
+	//if there is an error, store that for viewing.
+	var onError = function(reason) {
+		$scope.error = reason
+	}
 });
--- js/directive.js
+++ js/directive.js
@@ -1,13 +1,21 @@
 bandApp.directive("navigationbar", function() {
 	return {
-		templateUrl: "templates/nav.html",
+		templateUrl: "templates/directives/nav.html",
 		restrict: "E"
 	};
 });
 
 bandApp.directive("blackfooter", function() {
 	return {
-		templateUrl: "templates/footer.html",
+		templateUrl: "templates/directives/footer.html",
 		restrict: "E"
+	};
+});
+
+bandApp.directive("iTunesSearch", function() {
+	return {
+		templateUrl: "templates/directives/iTunesSearch.html",
+		restrict: "E",
+		scope: "true"
 	};
 });
