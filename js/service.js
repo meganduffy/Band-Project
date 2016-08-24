85b8e45447c3854df4b3724a68a62e3d650c47b8
Megan <meganemilyduffy@gmail.com>
2016-08-24 10:15:40 +0100

    tried out jsonp in services

--- js/service.js
+++ js/service.js
@@ -9,7 +9,11 @@ bandApp.factory("monkees", ["$http", function($http) {
 }]);
 
 bandApp.factory("monkeeitunes", ["$http", function($http) {
-	return $http.get("monkeeitunes.json")
+	return $http.jsonp("https://itunes.apple.com/lookup?id=4512388&entity=song&limit=15", {
+		params : {
+			"callback" : "JSON_CALLBACK"
+		}
+	})
 		.success(function(data) {
 			return data;
 		})
@@ -17,3 +21,14 @@ bandApp.factory("monkeeitunes", ["$http", function($http) {
 			return err;
 		});
 }]);
+
+// bandApp.factory("monkeeitunes", ["$http", function($http) {
+// 	return $http.jsonp("https://itunes.apple.com/lookup?id=4512388&entity=song&limit=15", {
+// 		params: {
+// 			"callback" : "JSON_CALLBACK"
+// 		}
+// 	})
+// 	.success(function(response) {
+// 		$scope.
+// 	})
+// }])
